ecea91232a8f9e5a6eba47d7230a104c1de10328
wswalker87 <scott@wscottwalker.com>
2024-02-14 21:33:40 -0600

    Need to complete the error handling.

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,78 +1,88 @@
-def arithmetic_arranger(problem_list, answers_visible=False):
-    # error_too_many = 'Too many problems.'
-    # error_wrong_operand = "Operator must be '+' or '-'."
+def exception_handler(operand1, operator, operand2):
+    # error_wrong_operand = "Operator must be '+' or '-'."**WORKS!**
+    try:
+        if operator != '+' and operator != '-':
+            raise BaseException
+    except:
+        return "Error: Operator must be '+' or '-'."
+    # error_max_digits = 'Numbers cannot be more than four digits.'**WORKS!**
+    try:
+        if len(operand1) >= 5 or len(operand2) >= 5:
+            raise BaseException
+    except:
+        return "Numbers cannot  be more than four digits."
     # error_only_digits = 'Number must only contain digits.'
-    # error_max_digits = 'Numbers cannot be more than four digits.'
-    # if len(problems) > 5:
-    #     current_error = error_too_many
-    # elif '*' or '/' in problem_list:
-    #     current_error = error_wrong_operand
-    # elif problem_list
-    #     return print(f'Error: {current_error}')
-    
+    try:
+        #if type(operand1) != int or type(operand2) != int:
+        if int(operand1) or int(operand2):
+            raise BaseException
+    except:
+        print("Error: Number must only contain digits")
+    return "winnah"
+
+def arithmetic_arranger(problem_list, answers_visible=False):
+   
     # Variable Definitions
     first = True
     width_between = '    '
     line1 = line2 = line3 = line4 = ''
-    split_problems = []
-    #Split input
+    # FCC placed a limit of 5 problems for the assignment. Check if over 5 exist.
+    try:
+        if len(problem_list) > 5:
+            raise BaseException
+    except:
+        return "Error: Too many problems"
+        exp = exception_handler(operand1, operator, operand2)
+        if exp != "winnah":
+            print(exp)
+    # Split input
     for problem in problem_list: #Split problem_list into individual problems
         split_problems = problem.split()
-        operand1 = split_problems[0] #Store first operand
-        operator = split_problems[1] #Store operator
-        operand2 = split_problems[2] #Store second operand
+        operand1 = split_problems[0] # Store first operand
+        operator = split_problems[1] # Store operator
+        operand2 = split_problems[2] # Store second operand
+        exp = exception_handler(operand1, operator, operand2)
+        if exp != "winnah":
+              print(exp)
         # End separation step
         # Begin conversion to int
         operand1 = int(operand1)
         operand2 = int(operand2)
+        # exp = exception_handler(operand1, operator, operand2)
+        # if exp != "winnah":
+        #     print(exp)
         # Create max width
         width = max(len(str(operand1)), len(str(operand2))) # Used the max func but converts to str because length only works on a string
-        # Arrangement
+        # First Arrangement
         if first == True:
             line1 += str(operand1).rjust(width + 2)
             line2 += operator + ' ' + str(operand2).rjust(width)
             line3 += '_' * (width + 2)
             if answers_visible == True:
                 if operator == '+':
-                    line4 += str(operand1 + operand2)##**LEFT OFF HERE. NEED TO GET THIS TO PRINT THE ANSWER AND FORMAT IT!
+                    line4 += str(operand1 + operand2).rjust(width + 2)
+                else:
+                    line4 += str(operand1 - operand2).rjust(width + 2)
+            first = False # Set to false to begin the loop over
+        else: # Begin the rest of the problems
+            line1 += str(operand1).rjust(width + 6)
+            line2 += operator.rjust(5) + ' ' + str(operand2).rjust(width)
+            line3 += width_between + "_" * (width + 2)
+            if answers_visible == True:
+                if operator == '+':
+                    line4 += width_between + str(operand1 + operand2).rjust(width + 2)
+                else:
+                    line4 += width_between + str(operand1 - operand2).rjust(width + 2)
+        if answers_visible == True:
+            #return line1 + '\n' + line2 + '\n' + line3 + '\n' + line4
+            print(f"{line1}\n{line2}\n{line3}\n{line4}")
+        else:
+            print(f"{line1}\n{line2}\n{line3}")
+            #return line1 + '\n' + line2 + '\n' + line3 + '\n'
 
+           
 
-
-    # #Assign each problem in problem_list to a variable
-    # problem1 = split_problems.pop(0)
-    # operand1_1 = int(problem1[0])
-    # operator1 = problem1[1]
-    # operand1_2 = int(problem1[2])
-    # solution1 = (operand1_1 + operand1_2)
-    # #prob_dict1 = {op1: problem1[0], operator1: problem1[1], op2: problem1[2]}
-    # #problem1 = [problem1.split(,)]
-    # problem2 = split_problems.pop(0)
-    # problem3 = split_problems.pop(0)
-    # problem4 = split_problems.pop(0)
-    # problem5 = split_problems.pop(0)
-
-
-    #for problem in split_problems:
-        #operand1, operator, operand2 = split_problems.split(' ')
-        #ind_list = [item.split(' ') for problem in split_problems]
-
-
-    #print(split_problems)
-    #print(operand1_1 + operand1_2)
-    #print(solution1)
-    #print(operator1)
-    #print(prob_dict1)
-    #print(problem_list)
-    #print(operand1 + operand2)
-    #print(operator)
-    #print(operand2)
-    #print(width)
-    print(line4)
-    
-    
-    
-    
-    #print(split_problems)
-
-arithmetic_arranger(["2 + 4 "])
-#arithmetic_arranger(["2 + 4 ", "8 + 16", "32 + 64", "128 + 256"])
+#arithmetic_arranger(["2 + 4 "])
+#arithmetic_arranger(["2 + 4 ", "8 - 16"])
+#arithmetic_arranger(["2 + 4 ", "8 - 16", "32 + 64"])
+arithmetic_arranger(["2 + 4 ", "8 - 16", "32 + 64", "128 - 256", "512 - 1024"])
